6a3f1aeabbb0560d6e2dd0930b860073d8ea2ae2
Kuzivashe <kuzibaye@gmail.com>
2023-03-28 19:02:54 -0700

    reload user updates

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -20,8 +20,8 @@ print(my_user)
 
 print("-- Create a new User 2 --")
 my_user2 = User()
-my_user2.first_name = "Peter"
-my_user2.email = "airbnb2@holbertonshool.com"
+my_user2.first_name = "John"
+my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
 print(my_user2)
